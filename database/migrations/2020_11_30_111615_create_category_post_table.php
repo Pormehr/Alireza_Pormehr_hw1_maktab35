269ee4033428887846819912da6261e722dbb335
paradise <pormehriust@gmail.com>
2020-11-30 14:50:58 +0330

    create category_post migration

---
+++ database/migrations/2020_11_30_111615_create_category_post_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCategoryPostTable extends Migration
+{
+    public function up()
+    {
+        Schema::create('category_post', function (Blueprint $table) {
+            $table->id();
+
+            $table->foreignId('category_id')
+                ->references('id')
+                ->on('categories')
+                ->cascadeOnDelete();
+
+            $table->foreignId('post_id')
+                ->references('id')
+                ->on('posts')
+                ->cascadeOnDelete();
+        });
+    }
+
+    public function down()
+    {
+        Schema::dropIfExists('category_post');
+    }
+}
