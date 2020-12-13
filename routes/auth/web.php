71a7b09b8d7182ea25badd39d86992b8194eba5c
pormehr <pormehriust@gmail.com>
2020-12-13 23:55:17 +0330

    create navbar

--- routes/auth/web.php
+++ routes/auth/web.php
@@ -4,7 +4,6 @@ use App\Http\Controllers\Auth\AuthController;
 use App\Http\Controllers\Auth\LoginController;
 use App\Http\Controllers\Auth\RegisterController;
 use App\Http\Controllers\Auth\VerificationController;
-use App\Http\Controllers\LogoutController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [AuthController::class, 'index'])->name('show.form');
