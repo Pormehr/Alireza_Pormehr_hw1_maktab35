ff54ae6432e5d50bb3ccc786b47e01dd2a821413
pormehr <pormehriust@gmail.com>
2020-12-14 14:13:19 +0330

    create customer change status process

--- routes/admin/web.php
+++ routes/admin/web.php
@@ -5,3 +5,5 @@ use Illuminate\Support\Facades\Route;
 Route::resource('', 'AdminController')->only(['index', 'edit', 'update']);
 
 Route::resource('customer', 'CustomerController')->only(['index', 'show', 'destroy']);
+
+Route::post('customer/{customer}', 'ChangeCustomerStatusController')->name('change.customer.status');
